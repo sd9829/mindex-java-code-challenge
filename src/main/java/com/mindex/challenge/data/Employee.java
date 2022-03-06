a1634e64cefb1e0046ff13659ec845203c67a482
Soumya Dayal <sd9829@rit.edu>
2022-03-05 22:30:41 -0500

    added code in Employee.java

--- src/main/java/com/mindex/challenge/data/Employee.java
+++ src/main/java/com/mindex/challenge/data/Employee.java
@@ -1,13 +1,20 @@
 package com.mindex.challenge.data;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Employee {
+    @Id
     private String employeeId;
     private String firstName;
     private String lastName;
     private String position;
     private String department;
+    @DBRef
     private List<Employee> directReports;
 
     public Employee() {
@@ -54,10 +61,72 @@ public class Employee {
     }
 
     public List<Employee> getDirectReports() {
-        return directReports;
+        return this.directReports == null ? Collections.emptyList() : this.directReports;
     }
 
     public void setDirectReports(List<Employee> directReports) {
         this.directReports = directReports;
+    }
+
+    public Employee employeeId(String employeeId) {
+        this.employeeId = employeeId;
+        return this;
+    }
+
+    public Employee firstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public Employee lastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public Employee position(String position) {
+        this.position = position;
+        return this;
+    }
+
+    public Employee department(String department) {
+        this.department = department;
+        return this;
+    }
+
+    public Employee directReports(List<Employee> directReports) {
+        this.directReports = directReports;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this)
+            return true;
+        if (!(object instanceof Employee)) {
+            return false;
+        }
+        Employee employee = (Employee) object;
+        return Objects.equals(employeeId, employee.employeeId)
+                && Objects.equals(firstName, employee.firstName)
+                && Objects.equals(lastName, employee.lastName)
+                && Objects.equals(position, employee.position)
+                && Objects.equals(department, employee.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employeeId, firstName, lastName, position, department, directReports);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " employeeId='" + getEmployeeId() + "'" +
+                ", firstName='" + getFirstName() + "'" +
+                ", lastName='" + getLastName() + "'" +
+                ", position='" + getPosition() + "'" +
+                ", department='" + getDepartment() + "'" +
+                ", directReports='" + getDirectReports() + "'" +
+                "}";
     }
 }
