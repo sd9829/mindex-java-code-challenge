939c1f770da9bc624e669ffac069581b0c3db184
Soumya Dayal <sd9829@rit.edu>
2022-03-05 22:02:53 -0500

    implemented equals func in Compensation.java

--- src/main/java/com/mindex/challenge/data/Compensation.java
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -3,6 +3,7 @@ package com.mindex.challenge.data;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.time.Instant;
+import java.util.Objects;
 
 public class Compensation {
 
@@ -37,5 +38,34 @@ public class Compensation {
 
     public void setEffectiveDate(Instant effectiveDate) {
         this.effectiveDate = effectiveDate;
+    }
+
+    public Compensation employee(Employee employee) {
+        this.employee = employee;
+        return this;
+    }
+
+    public Compensation salary(String salary) {
+        this.salary = salary;
+        return this;
+    }
+
+    public Compensation effectiveDate(Instant effectiveDate) {
+        this.effectiveDate = effectiveDate;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+        if (!(object instanceof Compensation)) {
+            return false;
+        }
+        Compensation compensation = (Compensation) object;
+        return Objects.equals(employee, compensation.employee)
+                && Objects.equals(salary, compensation.salary)
+                && Objects.equals(effectiveDate, compensation.effectiveDate);
     }
 }
